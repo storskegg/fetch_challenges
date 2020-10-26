f5a44bf7934780b812941847dfb49578c3830125
Liam Conrad <liam@storskegg.org>
2020-10-25 20:36:09 -0500

    first api

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/storskegg/fetch_chalenges
 
 go 1.15
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.1.17
+)
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"net/http"
 )
 
+type PyramidResponse struct {
+	Word          string `json:"word"`
+	IsPyramidWord bool   `json:"isPyramid"`
+}
+
 func main() {
-	fmt.Println("===================== [ Version ]======================")
-	fmt.Println(VersionStringCompare("2.1", "2.1.1"))
-	fmt.Println("-------------------------------------------------------")
-	fmt.Println("-------------------------------------------------------")
-	fmt.Println()
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	log.SetOutput(e.Logger.Output())
+
+	apiV1 := e.Group("/api/v1")
+	apiV1.GET("/pyramid/:word", func(c echo.Context) error {
+		word := c.Param("word")
+		if word == "" {
+			return echo.ErrNotFound
+		}
+
+		return c.JSON(http.StatusOK, &PyramidResponse{
+			Word:          word,
+			IsPyramidWord: IsPyramidWord(word),
+		})
+	})
+
+	err := e.Start("localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
